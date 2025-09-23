058bd07c3044a4e500f7ff69fe2b1ef0de0cfb14
ZEINEB-BJ <zeinebbenjeddou01@gmail.com>
2025-09-23 17:51:04 +0100

    traduction

--- bbc-front/src/app/admin/report-sales/report-sales.component.ts
+++ bbc-front/src/app/admin/report-sales/report-sales.component.ts
@@ -7,7 +7,7 @@ import { AdminService } from 'src/app/services/admin.service';
 @Component({
   selector: 'app-report-sales',
   templateUrl: './report-sales.component.html',
-  styleUrls: ['./report-sales.component.css']
+  styleUrls: ['./report-sales.component.css'],
 })
 export class ReportSalesComponent {
   report: ReportSales[] = [];
@@ -15,40 +15,59 @@ export class ReportSalesComponent {
   totalProfit: number = 0;
 
   today: Date = new Date();
-  startDate: string = new Date(this.today.getFullYear(), this.today.getMonth(), 2).toISOString().substring(0, 10);
-  endDate: string = new Date(this.today.getFullYear(), this.today.getMonth() + 1, 1).toISOString().substring(0, 10);
+  startDate: string = new Date(
+    this.today.getFullYear(),
+    this.today.getMonth(),
+    2
+  )
+    .toISOString()
+    .substring(0, 10);
+  endDate: string = new Date(
+    this.today.getFullYear(),
+    this.today.getMonth() + 1,
+    1
+  )
+    .toISOString()
+    .substring(0, 10);
 
-  constructor(
-    private adminService: AdminService
-  ) { }
+  constructor(private adminService: AdminService) {}
 
   ngOnInit(): void {
     this.getReport();
   }
 
   getReport() {
-    this.adminService.getSalesReport(this.startDate, this.endDate).subscribe((r) => {
-      this.report = r;
-      this.totalRevenue = 0;
-      this.totalProfit = 0;
-      this.report.forEach(i => {
-        this.totalRevenue += i.revenue;
-        this.totalProfit += i.profit;
+    this.adminService
+      .getSalesReport(this.startDate, this.endDate)
+      .subscribe((r) => {
+        this.report = r;
+        this.totalRevenue = 0;
+        this.totalProfit = 0;
+        this.report.forEach((i) => {
+          this.totalRevenue += i.revenue;
+          this.totalProfit += i.profit;
+        });
       });
-    });
   }
 
   generatePdf() {
     const doc = new jsPDF();
     doc.setFontSize(22);
-    doc.text("Sales Report", doc.internal.pageSize.getWidth() / 2, 15, { align: 'center' });
+    doc.text('Rapport de ventes', doc.internal.pageSize.getWidth() / 2, 15, {
+      align: 'center',
+    });
     doc.setFontSize(14);
-    doc.text(this.startDate + ' - ' + this.endDate, doc.internal.pageSize.getWidth() / 2, 24, { align: 'center' });
+    doc.text(
+      this.startDate + ' - ' + this.endDate,
+      doc.internal.pageSize.getWidth() / 2,
+      24,
+      { align: 'center' }
+    );
     autoTable(doc, {
       html: '#dataTable',
       theme: 'grid',
       startY: 28,
-      styles: { halign: 'center' }
+      styles: { halign: 'center' },
     });
     const pdfOutput = doc.output('blob');
     const url = URL.createObjectURL(pdfOutput);
