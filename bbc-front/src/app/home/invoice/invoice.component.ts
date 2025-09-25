7d9e5e80049f66e391dc253d01f75e92667feb9d
ZEINEB-BJ <zeinebbenjeddou01@gmail.com>
2025-09-25 21:52:17 +0100

    commit

--- bbc-front/src/app/home/invoice/invoice.component.ts
+++ bbc-front/src/app/home/invoice/invoice.component.ts
@@ -7,42 +7,19 @@ import { UtilService } from 'src/app/services/util.service';
 @Component({
   selector: 'app-invoice',
   templateUrl: './invoice.component.html',
-  styleUrls: ['./invoice.component.css'],
+  styleUrls: ['./invoice.component.css']
 })
 export class InvoiceComponent implements OnInit {
+
   customerName: string = '';
   customerEmail: string = '';
   customerAddress: string = '';
 
-  order: Order = {
-    id: 0,
-    orderDate: new Date().toISOString(),
-    customerId: 0,
-    subTotal: 0,
-    discount: 0,
-    tax: 0,
-    gatewayFee: 0,
-    shippingCharge: 0,
-    orderTotal: 0,
-    shippingStreet: '',
-    shippingCity: '',
-    shippingPostCode: '',
-    shippingState: '',
-    shippingCountry: '',
-    status: '',
-    paymentStatus: '',
-    paymentMethod: '',
-    cardNumber: '',
-    cardCvv: '',
-    cardHolderName: '',
-    cardExpiryDate: '',
-    orderDetails: [],
-  };
+  order!: Order;
 
   constructor(
     private customerService: CustomerService,
-    private route: ActivatedRoute
-  ) {}
+    private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.customerName = this.customerService.getCustomer().name;
